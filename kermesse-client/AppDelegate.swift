bc95c3683e6664dec48ecab212b6472e656b6020
Thomas Papiernik <thomas.papiernik@gmail.com>
2016-04-23 18:50:46 +0200

    Initial Commit

---
+++ kermesse-client/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  kermesse-client
+//
+//  Created by Thomas Papiernik on 23/04/2016.
+//  Copyright © 2016 Ysance. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ kermesse-clientTests/kermesse_clientTests.swift
@@ -0,0 +1,36 @@
+//
+//  kermesse_clientTests.swift
+//  kermesse-clientTests
+//
+//  Created by Thomas Papiernik on 23/04/2016.
+//  Copyright © 2016 Ysance. All rights reserved.
+//
+
+import XCTest
+@testable import kermesse_client
+
+class kermesse_clientTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
